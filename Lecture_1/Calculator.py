7d35e7cc01b47f7926ff211c0310a6d748e69fd2
JSL <alex.ermiloff2017@yandex.ru>
2025-09-09 10:22:18 +0300

    Executable calculator with polish notation

--- Lecture_1/Calculator.py
+++ Lecture_1/Calculator.py
@@ -5,7 +5,7 @@ def on_click(event):
     text = event.widget.cget("text")
     if text == "=":
         try:
-            result = eval(entry.get())
+            result = polish_notation(entry.get().replace("×","*"))
             entry.delete(0, tk.END)
             entry.insert(tk.END, str(result))
         except Exception as e:
@@ -18,7 +18,7 @@ def on_click(event):
 # Настройка окна
 root = tk.Tk()
 root.title("Калькулятор")
-root.geometry("400x500")
+root.geometry("400x600")
 root.resizable(False, False)
 root.configure(bg="#232323")
 
@@ -27,19 +27,85 @@ entry = tk.Entry(root, font=("Arial", 24), bg="#232323", fg="white", bd=0, justi
 entry.grid(row=0, column=0, columnspan=4, padx=10, pady=20, sticky="nsew")
 
 buttons = [
-"sqrt", "X²", "C", "/",
+"(", ")", "чето", "чето",
+"sqrt", "x²", "C", "/",
 "7", "8", "9", "×",
 "4", "5", "6", "-",
 "1", "2", "3", "+",
 "0", ".", "=",
 ]
 
-# Описание функционала символов и их приоритетов
-operators = {(1, "+", lambda x, y: x + y), (1, "-", lambda x, y: x - y),
-             (2, "×", lambda x, y: x * y), (2, "/", lambda x, y: x / y)}
+# Описание функций (символы и мат.операции) и их приоритетов
+operators = {"+": (1, lambda x, y: x + y), 
+             "-": (1, lambda x, y: x - y),
+             "*": (2, lambda x, y: x * y), 
+             "/": (2, lambda x, y: x / y)}
 
 # Обратная польская нотация
-def polish_notation():
+def polish_notation(expression):
+
+    #Парсер, принимает строку и отделяет числа от символов
+    def parser_numbers(expression):
+        number = ''
+        for symbal in expression:
+            # Место, где создается число
+            if symbal in '1234567890.':
+                number += symbal
+            # Если нечисловое значение, то заканчивается создание номера, происходит сброс
+            else:
+                if number:
+                    yield float(number)
+                    number = ''
+                if symbal in operators or symbal in '()':
+                    yield symbal
+        # Вывод числа при его создании
+        if number:
+            yield float(number)
+
+    # Парсер, принимает прошлое выражение и разносит функции по приоритету
+    def parser_functions(parsed_expression):
+        elements = []
+        for element in parsed_expression:
+            # Если выражение вне скобок, добавляем его справа от списка:
+            if element in operators:
+                 while (elements and elements[-1] != '(' and 
+                       operators[element][0] <= operators[elements[-1]][0]):
+                    yield elements.pop()
+                 elements.append(element)
+            # Если нашлась закрывающаяся скобка, проходимся по всем элементам в обратном порядке до открывающейся скобки    
+            elif element == ")":
+                while elements:
+                    check_bracket = elements.pop()
+                    if check_bracket == "(":
+                        break
+                    yield check_bracket
+            # Если скобка открывается, добавляется к списку элементов
+            elif element == "(":
+                elements.append(element)
+            # Вынос числовых значений вне списка    
+            else:
+                yield element
+        while elements:
+            yield elements.pop()
+    
+    # Функция подсчета на основе польской нотации
+    def calculation(polish_expression):
+        elements = []
+        for element in polish_expression:
+            # Если операция, то идет ее выполнение по инструкции
+            if element in operators:
+                y, x = elements.pop(), elements.pop()
+                elements.append(operators[element][1](x, y))
+            # Если число, то идет его фиксация для подсчета
+            else:
+                elements.append(element)
+        # Результат подсчета - единственный элемент списка
+        return elements[0]
+    
+    # Подсчет по польской нотации
+    return calculation(parser_functions(parser_numbers(expression)))
+    
+    
     
 
 def create_button(text):
