1a6da22d6ecb9a09f16053a9480e821923ca2edd
JSL <alex.ermiloff2017@yandex.ru>
2025-09-15 15:14:44 +0300

    Complete 7th task

---
+++ Lecture_2/Expression_7.py
@@ -0,0 +1,65 @@
+import tkinter as tk
+
+# Создание формы
+root = tk.Tk()
+root.title("Задание 3")
+root.geometry("350x400")
+root.resizable(False, False)
+root.configure(bg="#232323")
+
+# Рабочая область
+frame = tk.Frame(root, padx=10, pady=20)
+frame.pack(expand=True, fill="both", anchor="center")
+frame.configure(bg="#333333")
+
+# Стандартные настройки объектов
+label_a = tk.Label(frame, font=20, justify="center", bg="#333333", fg="white")
+label_a.configure(text="Введите a")
+label_b = tk.Label(frame, font=20, justify="center", bg="#333333", fg="white")
+label_b.configure(text="Введите b")
+label_c = tk.Label(frame, font=20, justify="center", bg="#333333", fg="white")
+label_c.configure(text="Введите c")
+label_d = tk.Label(frame, font=20, justify="center", bg="#333333", fg="white")
+label_d.configure(text="Введите d")
+label_answer = tk.Label(frame, font=20, justify="center",
+                        bg="#333333", fg="white")
+entry_a = tk.Entry(frame, justify="center", font=20)
+entry_b = tk.Entry(frame, justify="center", font=20)
+entry_c = tk.Entry(frame, justify="center", font=20)
+entry_d = tk.Entry(frame, justify="center", font=20)
+button = tk.Button(
+    frame,
+    justify="center",
+    font=20,
+    text="Есть ли треугольник",
+    command=lambda: find_numbers(int(entry_a.get()), int(entry_b.get()),
+                                 int(entry_c.get()), int(entry_d.get()))
+)
+
+# Размещение объектов в рабочей зоне
+label_a.grid(row=0, column=0, sticky="ew")
+entry_a.grid(row=0, column=1, pady=20, sticky="nsew")
+label_b.grid(row=1, column=0, sticky="ew")
+entry_b.grid(row=1, column=1, pady=20, sticky="nsew")
+label_c.grid(row=2, column=0, sticky="ew")
+entry_c.grid(row=2, column=1, pady=20, sticky="nsew")
+label_d.grid(row=3, column=0, sticky="ew")
+entry_d.grid(row=3, column=1, pady=20, sticky="nsew")
+label_answer.grid(row=4, column=0, columnspan=2, sticky="nsew")
+button.grid(row=5, column=0, columnspan=2, pady=10, sticky="nsew")
+
+
+# Место подсчета
+def find_numbers(a, b, c, d):
+    def F(fst, sec, trd):
+        if fst + sec >= trd and fst + trd >= sec and sec + trd >= fst:
+            return True
+        else:
+            return False
+    if F(a, b, c) or F(b, c, d) or F(a, b, d) or F(a, c, d):
+        label_answer.configure(text="YES")
+    else:
+        label_answer.configure(text="NO")
+
+
+root.mainloop()
