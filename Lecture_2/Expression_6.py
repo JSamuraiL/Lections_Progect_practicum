e891df0191cf72147fc740dbce14467a5e2d206c
JSL <alex.ermiloff2017@yandex.ru>
2025-09-15 12:12:32 +0300

    Complete 6th task

---
+++ Lecture_2/Expression_6.py
@@ -0,0 +1,60 @@
+import tkinter as tk
+
+# Создание формы
+root = tk.Tk()
+root.title("Задание 3")
+root.geometry("300x450")
+root.resizable(False, False)
+root.configure(bg="#232323")
+
+# Рабочая область
+frame = tk.Frame(root, padx=10, pady=20)
+frame.pack(expand=True, fill="both", anchor="center")
+frame.configure(bg="#333333")
+
+# Стандартные настройки объектов
+label_discripton = tk.Label(frame, font=20, justify="center", bg="#333333",
+                            fg="white")
+label_discripton.configure(text="Введите значение k")
+label_answer = tk.Listbox(frame, font=20, justify="center",
+                          bg="#333333", fg="white")
+entry = tk.Entry(frame, justify="center", font=20)
+button = tk.Button(
+    frame,
+    justify="center",
+    font=20,
+    text="Найти числа",
+    command=lambda: find_numbers(entry.get())
+)
+scrollbar = tk.Scrollbar(frame, orient="vertical", command=label_answer.yview)
+label_answer.configure(yscrollcommand=scrollbar.set)
+
+# Размещение объектов в рабочей зоне
+label_discripton.grid(row=0, column=0, columnspan=2, sticky="ew")
+entry.grid(row=1, column=0, columnspan=2, pady=20, sticky="nsew")
+label_answer.grid(row=2, column=0, sticky="nsew")
+scrollbar.grid(row=2, column=1, sticky="ns")
+button.grid(row=3, column=0, pady=10, sticky="nsew")
+
+
+# Место подсчета
+def find_numbers(k):
+    label_answer.delete(0, tk.END)
+    n_number = int(k)
+    number = 1
+    numbers = []
+    while number <= n_number:
+        summa = 0
+        n = 0
+        for n_element in str(number):
+            digit = int(n_element)
+            summa += digit
+            n += 1
+        if summa ** n == number:
+            numbers.append(number)
+        number = number + 1
+    for num in numbers:
+        label_answer.insert(tk.END, num)
+
+
+root.mainloop()
